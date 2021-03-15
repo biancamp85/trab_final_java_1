deff38f244bd1628adf831694926e3e6ac1d9f29
GabrielaMP00 <58892520+GabrielaMP00@users.noreply.github.com>
2021-03-15 14:09:04 -0300

    Update Principal.java

--- TrabalhoPOO1_src/controle/Principal.java
+++ TrabalhoPOO1_src/controle/Principal.java
@@ -1,12 +1,21 @@
 package controle;
 
+import modelo.ListaPacientes;
+import modelo.Paciente;
 import visao.TelaPrincipal;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		TelaPrincipal  principal = new TelaPrincipal();
-		principal.setVisible(true);
+		TelaPrincipal tp = new TelaPrincipal();
+		tp.setVisible(true);
+		
+		Paciente p = new Paciente();
+		
+		ListaPacientes lp = new ListaPacientes();
+		
+		GerarPacienteControle gpcon = new GerarPacienteControle(tp, p, lp);
+				
 	}
 }
